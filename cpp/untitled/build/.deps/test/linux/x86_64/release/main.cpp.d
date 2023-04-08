7afce96d50d772c36d75ce3ca0b75cecafb68ba2
dream727 <dream77070@163.com>
2023-04-08 14:41:26 +0800

    updata .gitignore

--- cpp/untitled/build/.deps/test/linux/x86_64/release/main.cpp.d
+++
@@ -1,6 +0,0 @@
-{
-    files = {
-        "main.cpp"
-    },
-    moduleinfo = "{\"version\":1,\"revision\":0,\"rules\":[{\"primary-output\":\"build\\/.objs\\/test\\/linux\\/x86_64\\/release\\/main.cpp.o\",\"requires\":[{\"logical-name\":\"test\"}],\"outputs\":[\"build\\/.gens\\/test\\/linux\\/x86_64\\/release\\/rules\\/modules\\/cache\\/574a3743\\/main.cpp.json\"]}]}"
-}
